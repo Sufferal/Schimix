a9ff36f572001b8986c224a9928cab1cd1648005
Ciprian <botnari_ciprian@mail.ru>
2022-12-25 14:25:15 +0200

    Add input & list and link favicon

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,36 @@
-console.log('yo');
+const inputAddElem = document.querySelector("#input-add");
+const btnAddElem = document.querySelector("#btn-add");
+
+const listElem = document.querySelector("#list");
+
+// Clear input on load
+window.onload = () => clearInput(inputAddElem);
+
+const addListItem = (e) => {  
+  // Prevent submit
+  e.preventDefault();
+
+  // Get input value
+  const inputAddValue = inputAddElem.value;
+
+  // Create, set text and append to list
+  const listItem = document.createElement("li");
+  listItem.textContent = inputAddValue;
+  listElem.appendChild(listItem);
+
+  // Clear and focus input
+  clearInput(inputAddElem);
+  inputAddElem.focus();
+};
+
+inputAddElem.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addListItem(e);
+  }
+});
+
+btnAddElem.addEventListener("click", addListItem);
+
+const clearInput = (elem) => {
+  elem.value = "";
+};
